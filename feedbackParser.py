f49ee24dddf1bd9a52ec102b9a3f563c3552bfc5
ismam <ishmamalhoque@gmail.com>
2021-11-11 14:25:13 +0800

    made small changes to feedbackParser.py

--- feedbackParser.py
+++ feedbackParser.py
@@ -6,8 +6,9 @@ entryfnc='computeDeriv'
 args='[[[4.5]],[[1.0,3.0,5.5]]]'
 command = f'python bin/clara feedback {path} incorrect/incorrect.py --entryfnc {entryfnc}' \
           f' --args {args} --ignoreio 1 --feedtype python'
-print(command)
+# print(command)
 command = command.split()
+subprocess.run('make')
 out = subprocess.run(command, capture_output=True)
 print(out.stderr.decode())
 print(out.stdout.decode())
