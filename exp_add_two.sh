20f0859128437601dc85f882609556cabdebef07
Ismam <ishmamalhoque@gmail.com>
2022-02-25 16:59:56 +0600

    test commit

---
+++ exp_add_two.sh
@@ -0,0 +1,3 @@
+clara cluster submissions/exp/term1/p1/a*.py --clusterdir clusters/exp/term1/p1 --entryfnc search_position \
+--args  "[[[1,3,5,6],7],[[1,3,5,6],0],[[1],0],[[1,3,5,6],5],[[1,3,5,6],2],[[1,3,8,11],13]]" \
+--ignoreio 1
