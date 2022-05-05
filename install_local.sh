1b021b072a922a089f1b62535e4241ceec9a6f81
Ismam Al Hoque <ishmamalhoque@gmail.com>
2022-05-05 17:46:50 +0800

    local install clara script with python 3.7

--- install_local.sh
+++ install_local.sh
@@ -1,4 +1,25 @@
-#make sure to use python 3.7 or less
+
+echo 'apt update'
+sudo apt update
+echo 'get build libraries'
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
+libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev libgdbm-dev libnss3-dev libedit-dev libc6-dev
+echo 'install python3.7'
+wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tgz
+cd Python-3.7.4
+./configure --enable-optimizations
+sudo ./configure --enable-optimizations  -with-lto  --with-pydebug
+sudo make altinstall
+cd ..
+echo 'removing python downloads'
+
+sudo rm -rf Python-3.7.4.tgz Python-3.7.4
+echo 'create virtualenv'
+rm -rf venv
+python3.7 -m pip install virtualenv --user
+python3.7 -m virtualenv venv
+. venv/bin/activate
 echo 'adding path to .bashrc'
 echo 'export LD_LIBRARY_PATH=/usr/lib/lp_solve/
 export PATH=$PATH:/home/ismam/.local/bin'>>~/.bashrc
